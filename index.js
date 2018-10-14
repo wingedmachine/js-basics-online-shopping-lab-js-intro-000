4999d22918146646831e085e0b6a6d8a98eaaded
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 20:28:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
   var cart = getCart();
-  return cart.push({itemName: item, itemPrice: Math.floor(Math.random()*100)});
+  cart.push({itemName: item, itemPrice: Math.floor(Math.random()*100)});
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
