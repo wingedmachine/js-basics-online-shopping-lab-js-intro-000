7cd868ee185c93d0b9eb74c4d5e4f97c12508254
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 21:02:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,5 +56,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if(!cardNumber) return "Sorry, we don't have a credit card on file for you."
 
+  setCart({});
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
