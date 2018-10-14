83461c08bc49b1e1b7985aa8654dfd3b3ccb36b4
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 21:00:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber.length === 0) return "Sorry, we don't have a credit card on file for you."
+  if(!cardNumber) return "Sorry, we don't have a credit card on file for you."
 
 
 }
